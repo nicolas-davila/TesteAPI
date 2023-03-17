e82372082299d0992246ad3e7d5c06be7cefd48a
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2023-03-17 17:30:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const uuid = require("uuid");
 
@@ -38,8 +39,44 @@ server.post("/to-do/:id", (req, res) => {
     const { id } = req.params;
     const { nome, descricao } = req.body;
 
-    const userExiste = users.find((user) => user.id === id)
+    const userExiste = users.find((user) => user.id === id);
+
+    if(!userExiste) {
+        res.status(404).json({ mensagem: "O usuário não existe!" })
+    }
+
+    const toDoId = uuid.v4();
+
+
+    const toDo = {
+        id: toDoId,
+        nome,
+        descricao,
+        criado_em: new Date(),
+        user_id: id
+    }
+
+    toDos.push(toDo);
+
+    res.status(201).json(toDo)
+
 })
+
+server.get("/to-do/:id", (req, res) => {
+
+    const { id } = req.params;
+
+    const userExiste = users.find((user) => user.id === id);
+
+    if(!userExiste) {
+        res.status(404).json({ mensagem: "O usuário não existe!" })
+    }
+
+    const userToDos = toDos.filter((todo) => todo.user.id === id);
+
+    res.json(userToDos);
+})
+
 
 server.get("/", (req, res) => {
     res.json("Aprendendo o Node.js com controllers!")
